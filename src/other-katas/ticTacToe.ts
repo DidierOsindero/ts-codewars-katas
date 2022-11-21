84844428c0653ba9151e971256ff1b469125f531
Didier Osindero <didierosindero@gmail.com>
2022-11-21 10:48:10 +0000

    Refactored

--- src/other-katas/ticTacToe.ts
+++ src/other-katas/ticTacToe.ts
@@ -2,16 +2,18 @@
 [[x,x,x][y,"",y]["","",""]]
 */
 
-type WinState = `X won by row` | 'X won by column' | 'X won by diagonal' | `Y won by row` | 'Y won by column' | 'Y won by diagonal' | 'There was a draw' | 'The game has not been finished yet';
+type WinState = `X won by row` | 'X won by column' | 'X won by diagonal' | `O won by row` | 'O won by column' | 'O won by diagonal' | 'There was a draw' | 'The game has not been finished yet';
+type Player = 'x' | 'o' | '';
 
-const calcWinState = (BoardState: string [][]): WinState => {
-    const xwonByRow = BoardState[0].every(el => el === 'x') || BoardState[1].every(el => el === 'x') || BoardState[2].every(el => el === 'x');
-    const xwonByColumn = BoardState.every(row => row[0] === 'x') || BoardState.every(row => row[1] === 'x') || BoardState.every(row => row[2] === 'x');
+const calcWinState = (BoardState: Player [][]): WinState => {
+    
+    const xwonByRow = BoardState.some(row => row.every(el => el ==='x')); //BoardState[0].every(el => el === 'x') || BoardState[1].every(el => el === 'x') || BoardState[2].every(el => el === 'x');
+    const xwonByColumn = BoardState.some((row, index) => row[index] === 'x'); //BoardState.every(row => row[0] === 'x') || BoardState.every(row => row[1] === 'x') || BoardState.every(row => row[2] === 'x');
     const xwonByDiagonal = BoardState[0][0] === 'x' && BoardState[1][1] === 'x' && BoardState[2][2] === 'x'|| BoardState [0][2] === 'x' && BoardState[1][1] === 'x' && BoardState[2][0] === 'x';
    
-    const ywonByRow = BoardState[0].every(el => el === 'y') || BoardState[1].every(el => el === 'y') || BoardState[2].every(el => el === 'y');
-    const ywonByColumn = BoardState.every(row => row[0] === 'y') || BoardState.every(row => row[1] === 'y') || BoardState.every(row => row[2] === 'y');
-    const ywonByDiagonal = BoardState[0][0] === 'y' && BoardState[1][1] === 'y' && BoardState[2][2] === 'y'|| BoardState [0][2] === 'y' && BoardState[1][1] === 'y' && BoardState[2][0] === 'y';
+    const ywonByRow = BoardState.some(row => row.every(el => el ==='o')); //BoardState[0].every(el => el === 'o') || BoardState[1].every(el => el === 'o') || BoardState[2].every(el => el === 'o');
+    const ywonByColumn = BoardState.some((row, index) => row[index] === 'o'); //BoardState.every(row => row[0] === 'o') || BoardState.every(row => row[1] === 'o') || BoardState.every(row => row[2] === 'o');
+    const ywonByDiagonal = BoardState[0][0] === 'o' && BoardState[1][1] === 'o' && BoardState[2][2] === 'o'|| BoardState [0][2] === 'o' && BoardState[1][1] === 'o' && BoardState[2][0] === 'o';
     
     const isDraw = BoardState.every(row => row.every(position => position !== ""));
 
@@ -23,11 +25,11 @@ const calcWinState = (BoardState: string [][]): WinState => {
     } else if (xwonByDiagonal) {
         return 'X won by diagonal'
     } else if (ywonByRow) {
-        return `Y won by row`
+        return `O won by row`
     } else if (ywonByColumn) {
-        return 'Y won by column'
+        return 'O won by column'
     } else if (ywonByDiagonal) {
-        return 'Y won by diagonal'
+        return 'O won by diagonal'
     } else if (isDraw) {
         return 'There was a draw'
     } else {
@@ -35,4 +37,4 @@ const calcWinState = (BoardState: string [][]): WinState => {
     }
 }
 
-console.log(calcWinState([['x','','x'],['x',"",'y'],["x","",""]]))
+console.log(calcWinState([['x','','x'],['x',"",'o'],["x","",""]]))
