9b7865f1872cd022d4699e69c9c1b6767dc8071a
Didier Osindero <didierosindero@gmail.com>
2022-11-21 10:56:08 +0000

    Refactored properly

--- src/other-katas/ticTacToe.ts
+++ src/other-katas/ticTacToe.ts
@@ -1,18 +1,14 @@
-/*
-[[x,x,x][y,"",y]["","",""]]
-*/
-
 type WinState = `X won by row` | 'X won by column' | 'X won by diagonal' | `O won by row` | 'O won by column' | 'O won by diagonal' | 'There was a draw' | 'The game has not been finished yet';
 type Player = 'x' | 'o' | '';
 
 const calcWinState = (BoardState: Player [][]): WinState => {
     
     const xwonByRow = BoardState.some(row => row.every(el => el ==='x')); //BoardState[0].every(el => el === 'x') || BoardState[1].every(el => el === 'x') || BoardState[2].every(el => el === 'x');
-    const xwonByColumn = BoardState.some((row, index) => row[index] === 'x'); //BoardState.every(row => row[0] === 'x') || BoardState.every(row => row[1] === 'x') || BoardState.every(row => row[2] === 'x');
+    const xwonByColumn = BoardState.every(row => row[0] === 'x') || BoardState.every(row => row[1] === 'x') || BoardState.every(row => row[2] === 'x');
     const xwonByDiagonal = BoardState[0][0] === 'x' && BoardState[1][1] === 'x' && BoardState[2][2] === 'x'|| BoardState [0][2] === 'x' && BoardState[1][1] === 'x' && BoardState[2][0] === 'x';
    
     const ywonByRow = BoardState.some(row => row.every(el => el ==='o')); //BoardState[0].every(el => el === 'o') || BoardState[1].every(el => el === 'o') || BoardState[2].every(el => el === 'o');
-    const ywonByColumn = BoardState.some((row, index) => row[index] === 'o'); //BoardState.every(row => row[0] === 'o') || BoardState.every(row => row[1] === 'o') || BoardState.every(row => row[2] === 'o');
+    const ywonByColumn = BoardState.every(row => row[0] === 'o') || BoardState.every(row => row[1] === 'o') || BoardState.every(row => row[2] === 'o');
     const ywonByDiagonal = BoardState[0][0] === 'o' && BoardState[1][1] === 'o' && BoardState[2][2] === 'o'|| BoardState [0][2] === 'o' && BoardState[1][1] === 'o' && BoardState[2][0] === 'o';
     
     const isDraw = BoardState.every(row => row.every(position => position !== ""));
@@ -37,4 +33,4 @@ const calcWinState = (BoardState: Player [][]): WinState => {
     }
 }
 
-console.log(calcWinState([['x','','x'],['x',"",'o'],["x","",""]]))
+console.log(calcWinState([['o','o','x'],['x',"",'o'],["x","o",""]]))
