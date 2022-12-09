0f1b4a4968f44ac6fd0ac64731e036548ae99396
Didier Osindero <didierosindero@gmail.com>
2022-12-09 10:03:35 +0000

    Solve writeMap

---
+++ src/other-katas/writeMap.test.ts
@@ -0,0 +1,8 @@
+import { myMap } from "./writeMap";
+
+test("myMap maps through an array and returns a new array with values transformed by the callback", () => {
+    expect(myMap(["H"], el => el + 'i')).toEqual(['Hi']);
+    expect(myMap(['Jim', 'Tom'], el => el + ' Jameson')).toEqual([ 'Jim Jameson', 'Tom Jameson' ]);
+    expect(myMap([1, 2, 3], el => el * 2)).toEqual([ 2, 4, 6]);
+    expect(myMap([], el => el + 'i')).toEqual([]);
+  });
---
+++ src/other-katas/writeMap.ts
@@ -0,0 +1,10 @@
+export const myMap =  <I, O>(inputArr: I[], callback:(element: I) => O): O[] => {
+    let resultArr: O[] = [];
+    for (const el of inputArr) {
+        resultArr.push(callback(el))
+    }
+    return resultArr;
+}
+
+const arr1 = ['Jim', 'Tom']
+console.log(myMap(arr1, (el) => el + ' Jameson'))
