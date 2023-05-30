ff59004e5b84b5c1a7246ed08202c110dbd51151
Didier Osindero <didierosindero@gmail.com>
2023-05-30 19:03:46 +0100

    Create versions with ICell (where Cell status is also stored)

---
+++ src/leetcode/36-valid-sudoku/isValidSudoku2.ts
@@ -0,0 +1,54 @@
+export interface ICell {
+  val: string;
+  status: "pre-defined" | "user-defined";
+}
+
+export function isValidSudoku2(board: ICell[][]): boolean {
+  return board.every((row, rowIdx) => {
+    return row.every((cell, columnIdx) => {
+      return isValidCell2(board, rowIdx, columnIdx);
+    });
+  });
+}
+
+export function isValidCell2(
+  board: ICell[][],
+  row: number,
+  column: number
+): boolean {
+  const cellToValidate = board[row][column];
+  if (cellToValidate.val === ".") return true;
+  let squareCurrentRow = Math.floor(row / 3) * 3;
+  let squareCurrentColumn = Math.floor(column / 3) * 3;
+
+  for (let i = 0; i < 9; i++) {
+    //Going down a column
+    if (i !== row) {
+      const currentCell = board[i][column];
+      if (currentCell.val === cellToValidate.val) return false;
+    }
+
+    //Going along a row
+    if (i !== column) {
+      const currentCell = board[row][i];
+      if (currentCell.val === cellToValidate.val) return false;
+    }
+
+    //Skip cell to validate and check if there number is duplicated in square
+    const currentCellInSquare = board[squareCurrentRow][squareCurrentColumn];
+    const isSamePosition =
+      squareCurrentColumn === column && squareCurrentRow === row;
+    if (currentCellInSquare.val === cellToValidate.val && !isSamePosition) {
+      return false;
+    }
+
+    //If the next column is outside the square, skip to next row and first column in this square
+    if (!((squareCurrentColumn + 1) % 3)) {
+      squareCurrentRow++;
+      squareCurrentColumn = Math.floor(column / 3) * 3;
+    } else {
+      squareCurrentColumn++;
+    }
+  }
+  return true;
+}
---
+++ src/leetcode/37-sudoku-solver/solveSudoku2.ts
@@ -0,0 +1,179 @@
+import {
+  ICell,
+  isValidCell2,
+  isValidSudoku2,
+} from "../36-valid-sudoku/isValidSudoku2";
+
+export function solveSudoku2(
+  board: ICell[][],
+  row: number = 0,
+  column: number = 0
+): boolean {
+  // Base case => return if last board[row][column] on board is reached and it is valid
+  if (row === 8 && column === 8 && board[row][column].val !== ".") {
+    return isValidCell2(board, row, column);
+  }
+
+  //Find next empty slot
+  while (board[row][column].val !== "." && !(row === 8 && column === 8)) {
+    const result = nextRowColumn(row, column);
+    row = result[0];
+    column = result[1];
+  }
+
+  //Have to backtrack if path is incorrect
+  for (let candidate = 1; candidate < 10; candidate++) {
+    board[row][column].val = candidate.toString();
+    if (isValidCell2(board, row, column)) {
+      //Check if it is a valid last cell
+      if (row === 8 && column === 8) return true;
+
+      //if somewhere further along the path does not work (returns false)
+      // reset board[row][column].val to "."
+      if (
+        solveSudoku2(
+          board,
+          nextRowColumn(row, column)[0],
+          nextRowColumn(row, column)[1]
+        ) === false
+      ) {
+        board[row][column].val = ".";
+        board[row][column].status = "user-defined";
+      } else return true;
+    }
+  }
+
+  if (row === 0 && column === 0) return true;
+  board[row][column].val = ".";
+  board[row][column].status = "user-defined";
+  return false;
+}
+
+function nextRowColumn(row: number, column: number): number[] {
+  if (column === 8) return [row + 1, 0];
+  return [row, column + 1];
+}
+
+const board: ICell[][] = [
+  [
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: "9", status: "pre-defined" },
+    { val: "7", status: "pre-defined" },
+    { val: "4", status: "pre-defined" },
+    { val: "8", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+  ],
+  [
+    { val: "7", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+  ],
+  [
+    { val: ".", status: "user-defined" },
+    { val: "2", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: "1", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: "9", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+  ],
+  [
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: "7", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: "2", status: "pre-defined" },
+    { val: "4", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+  ],
+  [
+    { val: ".", status: "user-defined" },
+    { val: "6", status: "pre-defined" },
+    { val: "4", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: "1", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: "5", status: "pre-defined" },
+    { val: "9", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+  ],
+  [
+    { val: ".", status: "user-defined" },
+    { val: "9", status: "pre-defined" },
+    { val: "8", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+    { val: "3", status: "pre-defined" },
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+  ],
+  [
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: "8", status: "pre-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: "3", status: "pre-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: "2", status: "pre-defined" },
+
+    { val: ".", status: "user-defined" },
+  ],
+  [
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: "6", status: "pre-defined" },
+  ],
+  [
+    { val: ".", status: "user-defined" },
+    { val: ".", status: "user-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: "2", status: "pre-defined" },
+
+    { val: "7", status: "pre-defined" },
+
+    { val: "5", status: "pre-defined" },
+
+    { val: "9", status: "pre-defined" },
+
+    { val: ".", status: "user-defined" },
+
+    { val: ".", status: "user-defined" },
+  ],
+];
+console.log(solveSudoku2(board), board, isValidSudoku2(board));
